31093ac1ffc031c45c2c2a54e1063397d643094a
Ron Tapia <rdt12+git@psu.edu>
2020-03-09 16:04:21 -0400

    Added argument parsing.

--- Utils.py
+++ Utils.py
@@ -2,15 +2,22 @@ from scimma.client import models
 from scimma.client import publish
 from scimma.client import io
 
-messageCount = 1
+messageCount   = 1
+scimmaConfFile = ""
+scimmaUrl      = ""
 
+##
+## There is no way to pass the GCN handler arguments, so using global variables.
+## For now, one connection per event. Eventually, keep the connection to the
+## SCiMMA server open.
+##
 def writeToScimma(payload, root):
+    global messageCount
+    global scimmaUrl
+    global scimmaConfFile
     voevent = models.VOEvent.from_xml(payload)
-    broker = "kafka://scimma-server:9092/gcn"
-    mode   = "w"
-    config = "/root/shared/kafkacat.conf"
-    s = io.Stream(config=config, format="json")
-    sh = s.open(broker, mode=mode, format="json")
+    s = io.Stream(config=scimmaConfFile, format="json")
+    sh = s.open(scimmaUrl, mode="w", format="json")
     sh.write(voevent.asdict())
     sh.close()
     print("Sent message %d" % messageCount)
