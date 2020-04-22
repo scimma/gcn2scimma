8331b672e828675e862cd31dd522a21b8f62cc72
Ron Tapia <rdt12@psu.edu>
2020-04-22 10:16:15 -0400

    rename work

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_requires = ["pygcn", "scimma-client >= 0.0.4"]
+install_requires = ["pygcn", "hop-client >= 0.0.4"]
 
 setuptools.setup(
-    name="scimma-gcn2scimma",
+    name="scimma-gcn2hop",
     version="0.0.1",
     author="Ron Tapia",
     author_email="rdt12@psu.edu",
-    description="Read from GCN/TAM and write to SCiMMA",
+    description="Read from GCN/TAM and write to HOPSKOTCH",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/scimma/gcn2scimma",
+    url="https://github.com/scimma/gcn2hop",
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
