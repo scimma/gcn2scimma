11ca33ccd29a9c7d06d1c1bc90ae68cb3d98cea9
Ron Tapia <rdt12@psu.edu>
2020-04-27 11:13:05 -0400

    removed extraneous double quote

--- scripts/mitest.sh
+++ scripts/mitest.sh
@@ -2,4 +2,5 @@
 ###
 ### Run mi.sh in the scimma/base container.
 ###
-docker run --rm -d -v `pwd`:/install  scimma/base:0.1.1 /install/scripts/mi.sh"
+docker run --rm -d -v `pwd`:/install  scimma/base:0.1.1 /install/scripts/mi.sh
+
