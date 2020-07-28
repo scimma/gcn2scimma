1fb51e721588be2c13903faeef6d1a34f14b87f1
shereenelsayed <shereenhussein92@gmail.com>
2020-07-28 00:03:35 +0000

    linting

--- scripts/tns_run.py
+++ scripts/tns_run.py
@@ -1,17 +1,17 @@
 import os
 from stream2hop import Utils as ut
-import json 
+import json
 
 region = "us-west-2"
 cred_secret_name = "dev-gcn2hop-hopcreds"
 tns_api_key_secret_name = "tns_api_key"
 configDir = "/root/share"
-Location  = "%s/kafkacat.conf" % configDir
+Location = "%s/kafkacat.conf" % configDir
 
 
 os.system("mkdir -p %s" % configDir)
 creds = ut.get_secret(cred_secret_name)
-if creds != None:
+if creds is not None:
     creds_json = json.loads(creds)
     values = creds_json["creds"].split(":")
     creds = {"user": values[0], "pass": values[1]}
--- scripts/tns_run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd /install
-pip3 install -e .
-stream2hop tns --api_key {{secrets.API_Key}}
