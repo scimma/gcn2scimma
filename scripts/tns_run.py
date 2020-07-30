a631e0e6b7a79ed89881eab4ff3343e407b8e6f7
shereenelsayed <shereenhussein92@gmail.com>
2020-07-30 01:42:01 +0000

    linting

--- scripts/tns_run.py
+++ scripts/tns_run.py
@@ -6,19 +6,19 @@ region = "us-west-2"
 cred_secret_name = "dev-gcn2hop-hopcreds"
 tns_api_key_secret_name = "tns_api_key"
 config_dir = "/root/share"
-location = "%s/kafkacat.conf" % configDir
+location = "%s/kafkacat.conf" % config_dir
 
 
-os.system(f"mkdir -p {configDir}")
+os.system(f"mkdir -p {config_dir}")
 creds = ut.get_secret(cred_secret_name)
 if creds is not None:
     creds_json = json.loads(creds)
     values = creds_json["creds"].split(":")
     creds = {"user": values[0], "pass": values[1]}
-    ut.writeConfig(Location, creds)
-    
+    ut.writeConfig(location, creds)
+
     tns_api_key = json.loads(ut.get_secret(tns_api_key_secret_name))["key"]
-    run = os.system(f"stream2hop tns -F {Location} --api_key={tns_api_key}")
+    run = os.system(f"stream2hop tns -F {location} --api_key={tns_api_key}")
 
 else:
     print("Error: Credentials cannot be extracted")
